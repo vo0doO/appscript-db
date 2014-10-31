7eb06ecb7140eb9da1be685db4374c7f6157360d
Joel Colucci <JoelColucci@Gmail.com>
2014-10-31 13:19:01 -0400

    Factored out Sheet methods into Sheet object

--- ssDB.js
+++ ssDB.js
@@ -8,23 +8,42 @@ function SSDB(dbId) {
   var _sheets = {};
   
   this.getSheet = function(sheetName) {
-    if (!sheetName) throw new Error("Invalid number of arguments");
-    
+    if (!sheetName) throw new Error("This is an extended error message");
+ 
     if (_sheets.hasOwnProperty(sheetName)) {
+      Logger.log("Got cached sheet");
       return _sheets[sheetName];
     }
-    return _sheets[sheetName] = _ss.getSheetByName(sheetName);
+    return _sheets[sheetName] = new Sheet(_ss, sheetName);
   };
+ 
+}
+
+function Sheet(_ss, sheetName) {
+  var _sheet = _ss.getSheetByName(sheetName);
+  var _range = _sheet.getDataRange();
+  var _data = _range.getValues();
   
-  this.select = function(sheetName, primaryKey, colIndex) {
-    var data = this.getSheet(sheetName).getDataRange().getValues();
+  this.select = function(primaryKey, colIndex) {
     var colIndex = colIndex || 0;
-    
-    for (var i = 0, n = data.length; i < n; i++) {
-      if (data[i][colIndex] == primaryKey) {
-        return data[i];
+    for (var i = 0, n = _data.length; i < n; i++) {
+      if (_data[i][colIndex] == primaryKey) {
+        return _data[i];
       }
     }
-    return false;
+    return _data;
   };
+
+  this.insert = function(data) {
+    // TODO: generate unique id
+    
+    // TODO: add to data array
+    
+    // atomic append to sheet
+    _sheet.appendRow(data);
+    
+    // refresh object cache
+    _range = _sheet.getDataRange();
+    _data = _range.getValues();
+  }  
 }
